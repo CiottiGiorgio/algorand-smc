5e59489006cf24a2bd6ef647bc3e18b9b5fb0d1e
CiottiGiorgio <gciotti.dev@gmail.com>
2023-02-03 12:46:47 +0100

    Changed demo script according to new setupProposal protobuf type.

--- algorandsmc/recipient.py
+++ algorandsmc/recipient.py
@@ -1,12 +1,20 @@
 import asyncio
 
 import websockets
+from algosdk.encoding import is_valid_address
 
 from algorandsmc.smc_pb2 import setupProposal, SMCMethod
 
 
+RECIPIENT_ADDR = "QPKVS55LXLVR5AGHLE4B5T5HTXJSFP5ZBVEWVN5DGPC4FV267AVU35ZZEI"
+
+
 async def setup_channel(websocket):
     setup_proposal = setupProposal.FromString(await websocket.recv())
+    # Protobuf doesn't know what constitutes a valid Algorand address.
+    if not is_valid_address(setup_proposal.sender):
+        raise ValueError
+    
     print(setup_proposal)
 
 
--- algorandsmc/sender.py
+++ algorandsmc/sender.py
@@ -5,9 +5,12 @@ import websockets
 from algorandsmc.smc_pb2 import SMCMethod, setupProposal
 
 
+SENDER_ADDR = "DWV4424IVCT6JGNFG33ZVRGYYTQHHF2MZOLA7NG5CLIO6AXETJZQMI7JAU"
+
+
 async def sender(websocket):
     await websocket.send(SMCMethod(method=SMCMethod.MethodEnum.SETUP_CHANNEL).SerializeToString())
-    await websocket.send(setupProposal(nonce=1024, minRefundBlock=10_000, maxRefundBlock=11_000).SerializeToString())
+    await websocket.send(setupProposal(sender=SENDER_ADDR, nonce=1024, minRefundBlock=10_000, maxRefundBlock=11_000).SerializeToString())
 
 
 async def main():
