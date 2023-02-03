e70b897b48e4c9d2b78ce594e7447ad347230477
CiottiGiorgio <gciotti.dev@gmail.com>
2023-02-03 18:15:01 +0100

    Template for ALGO SMC payment.

---
+++ algorandsmc/sigtemplates/paytxn.py
@@ -0,0 +1,8 @@
+"""
+This file implements the multisignature transaction that A uses to pay B on the SMC.
+"""
+from algosdk.transaction import PaymentTxn, SuggestedParams
+
+
+def smc_pay(sender: str, msig: str, recipient: str, amount: int, sp: SuggestedParams) -> PaymentTxn:
+    return PaymentTxn(msig, sp, recipient, amount, close_remainder_to=sender)
